29093beaf32b5c9a8197d1b26071fd816d9f14ee
Zhuohao-Li <garrick0508@gmail.com>
2025-11-24 14:19:02 -0800

    fix opt

--- scripts/run-qwen3-30B-A3B-fsdp.sh
+++ scripts/run-qwen3-30B-A3B-fsdp.sh
@@ -71,9 +71,9 @@ OPTIMIZER_ARGS=(
    --adam-beta1 0.9
    --adam-beta2 0.98
 
-   --optimizer-cpu-offload
-   --overlap-cpu-optimizer-d2h-h2d
-   --use-precision-aware-optimizer
+   # --optimizer-cpu-offload
+   # --overlap-cpu-optimizer-d2h-h2d
+   # --use-precision-aware-optimizer
 )
 
 WANDB_ARGS=(
