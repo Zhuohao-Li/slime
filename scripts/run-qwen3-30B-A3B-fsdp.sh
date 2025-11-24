092c005d82598000f4c9f613f8a22c9cb36b4f3c
Zhuohao-Li <garrick0508@gmail.com>
2025-11-24 15:47:29 -0800

    mem frac

--- scripts/run-qwen3-30B-A3B-fsdp.sh
+++ scripts/run-qwen3-30B-A3B-fsdp.sh
@@ -85,10 +85,10 @@ WANDB_ARGS=(
 
 SGLANG_ARGS=(
    --rollout-num-gpus-per-engine 8
-   --sglang-mem-fraction-static 0.5
+   --sglang-mem-fraction-static 0.4
    --sglang-decode-log-interval 1000
    --sglang-chunked-prefill-size 4096
-   --sglang-cuda-graph-max-bs 512
+   --sglang-cuda-graph-max-bs 256
 )
 
 FSDP_ARGS=(
