d1e95d4cc9b5f0199eabf3446bd1a9026e0417fc
Zhuohao-Li <garrick0508@gmail.com>
2025-12-17 17:39:53 -0800

    ep new

--- examples/geo3k_vlm/run_geo3k_vlm.sh
+++ examples/geo3k_vlm/run_geo3k_vlm.sh
@@ -122,7 +122,6 @@ OPTIMIZER_ARGS=(
 SGLANG_ARGS=(
    --rollout-num-gpus-per-engine 8
    --sglang-mem-fraction-static 0.7
-   --sglang-expert-parallel-size 8
    --sglang-cuda-graph-bs 1 2 4 8 16 24 32 40 48 56 64 72 80 88 96 104 112 120 128 136 144 152 160 168 176 184 192 200 208 216 224 232 240 248 256
 )
 
